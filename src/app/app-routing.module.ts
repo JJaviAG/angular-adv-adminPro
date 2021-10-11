d2eeafb352d39ef0b40e151b210970497e9fcb74
José javier Arriaza García <josejavierarriaza@gmail.com>
2021-10-11 15:35:05 +0200

    section-4 - routing y modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,59 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
+import { AuthRoutingModule } from './auth/auth-routing.module';
+
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NoPageFoundComponent } from './pages/no-page-found/no-page-found.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
+
+import { PagesRoutingModule } from './pages/pages.routing.module';
+
 
 const routes: Routes = [
-    {
-        path: '',
-        component: PagesComponent,
-        children:[
-            {
-                path: 'dashboard',
-                component: DashboardComponent
-            },
-            {
-                path: 'progress',
-                component: ProgressComponent
-            },
-            {
-                path: 'grafica1',
-                component: Grafica1Component
-            },
-            {
-                path: '',
-                redirectTo: '/dashboard',
-                pathMatch: 'full'
-            },
-        ]
-    },
-  
-    {
-        path: 'register',
-        component: RegisterComponent
-    },
-    {
-        path: 'login',
-        component: LoginComponent
-    },
-
-
-    
-    {
-        path: '**',
-        component: NoPageFoundComponent
-    },
+    //path: '/auth', AuthRouting
+    //path: /pages, PagesRouting
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    { path: '**', component: NoPageFoundComponent },
 ]
 
 @NgModule({
     declarations: [],
     imports: [
         RouterModule.forRoot(routes),
+        PagesRoutingModule,
+        AuthRoutingModule
     ], exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,28 @@
+//modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-
+import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
+//components
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NoPageFoundComponent } from './pages/no-page-found/no-page-found.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { PagesComponent } from './pages/pages.component';
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
+
+
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
     NoPageFoundComponent,
-    DashboardComponent,
-    BreadcrumsComponent,
-    HeaderComponent,
-    SidebarComponent,
-    ProgressComponent,
-    Grafica1Component,
-    PagesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    PagesModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
